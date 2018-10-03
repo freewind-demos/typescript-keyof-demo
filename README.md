f2484e62ce2b21e1c16f695062eb9e343d0b8453
freewind <nowindlee@gmail.com>
2018-10-03 21:00:09 +0800

    demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript Hello World Demo
-============================
+TypeScript "keyof" Demo
+=======================
+
+使用`keyof`加上`typeof`，可以限制某个参数的类型必须为已知某个object的key或者value。
 
 ```
 npm install
@@ -7,3 +9,5 @@ npm run demo
 ```
 
 It will print `Hello, TypeScript!`
+
+![demo](./images/demo.jpg)
--- hello.ts
+++ hello.ts
@@ -1,3 +1,17 @@
-import {capitalize} from 'lodash';
+const user = {
+  name: 'javascript',
+  age: 3
+};
 
-console.log("Hello " + capitalize("typescript") + "!");
+type UserType = typeof user
+
+function modifyUser<T extends keyof UserType>(key: T, value: UserType[T]) {
+  user[key] = value
+}
+
+modifyUser('name', 'typescript');
+modifyUser('age', 100);
+
+modifyUser('name', 123);
+modifyUser('age', 'xxx');
+modifyUser('unknown', 100);
